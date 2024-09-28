3cbb00e44ed14cb81f0504fb1d28ca0ff01d4e8e
pgattic <pgattic@gmail.com>
2024-09-28 17:59:16 -0600

    Handle error cases

--- src/citation.rs
+++ src/citation.rs
@@ -26,42 +26,65 @@ pub struct Verse {
 }
 
 /// This function searches the user's canon directory for the reference given.
-pub fn cite(path: &PathBuf, reference: &Reference) -> Result<Citation, String> {
+pub fn cite(path: &PathBuf, reference: &Reference) -> Result<Citation, &'static str> {
 
-    let book_path = find_book(path, &reference.book).unwrap();
+    let book_path = find_book(path, &reference.book)?;
 
     let mut res_chs: Vec<Chapter> = vec![];
 
     for ref_ind in &reference.indications {
         let ch_path = book_path.join(&ref_ind.chapter);
-        res_chs.push(Chapter::from_ch_reference(ch_path, &ref_ind.verse_ranges));
+        // Throw away invalid chapters
+        match Chapter::from_ch_reference(ch_path, &ref_ind.verse_ranges) {
+            Ok(chapter) => {
+                res_chs.push(chapter);
+            }
+            Err(_) => ()
+        }
     }
 
     Ok(
-        Citation { chapters: res_chs}
+        Citation { chapters: res_chs }
     )
 }
 
-pub fn find_book(path: &PathBuf, reference: &str) -> Result<PathBuf, String> {
+pub fn find_book(path: &PathBuf, reference: &str) -> Result<PathBuf, &'static str> {
 
     // Directory where Canon stores its texts and global config
     let texts_path = path.join("texts");
     
     // Read and marshal Canon's global config file
-    let config_contents = fs::read_to_string(texts_path.join("config.json")).unwrap();
-    let config = GlobalConfig::from_str(config_contents).unwrap();
+    let config = match fs::read_to_string(texts_path.join("config.json")) {
+        Ok(data) => match GlobalConfig::from_str(data) {
+            Ok(config) => config,
+            Err(_) => {
+                return Err("Malformed Canon config");
+            }
+        }
+        Err(_) => {
+            return Err("Canon Config not set up");
+        }
+    };
 
     // Search the installed canons in order of their priority
     for dirname in config.priority {
         let text_path = texts_path.join(dirname);
 
         // Read and marshal the canon's config (stores book aliases)
-        let text_config_contents = fs::read_to_string(text_path.join("config.json"))
-            .unwrap();
-        let canon_config = CanonConfig::from_str(text_config_contents).unwrap();
+        let pkg_config = match fs::read_to_string(text_path.join("config.json")) {
+            Ok(data) => match PackageConfig::from_str(data) {
+                Ok(config) => config,
+                Err(_) => {
+                    return Err("Malformed Package config");
+                }
+            }
+            Err(_) => {
+                return Err("Missing Package Config");
+            }
+        };
 
         // See if any of the aliases match the input reference
-        for (book, aliases) in canon_config.aliases {
+        for (book, aliases) in pkg_config.aliases {
             // Case-insensitive search
             let lowers: Vec<String> = aliases
                 .into_iter()
@@ -73,18 +96,27 @@ pub fn find_book(path: &PathBuf, reference: &str) -> Result<PathBuf, String> {
             }
         }
     }
-    Err("Book not found".to_string())
+    Err("Book not found")
 }
 
 impl Chapter {
-    pub fn from_ch_reference(ch_path: PathBuf, references: &Vec<RefVerse>) -> Self {
+    pub fn from_ch_reference(ch_path: PathBuf, references: &Vec<RefVerse>) -> Result<Self, &'static str> {
         let mut result = Self {
             name: "".to_string(),
             path: ch_path.clone(),
             verses: vec![],
         };
 
-        let ch_file = fs::read_to_string(&ch_path).unwrap();
+        if let Err(_) = fs::exists(&ch_path) {
+            return Err("Chapter not found");
+        }
+        let ch_file = match fs::read_to_string(&ch_path) {
+            Ok(data) => data,
+            Err(_) => {
+                return Err("Could not read chapter file")
+            }
+        };
+
         let split: &Vec<&str> = &ch_file.split('\n').collect();
 
         for r in references {
@@ -119,7 +151,7 @@ impl Chapter {
                 }
             }
         }
-        result
+        Ok(result)
     }
 }
 
