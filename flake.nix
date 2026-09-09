eae45840c727d1172abd0c8b72608c99da2af47c
Preston Corless <pgattic@gmail.com>
2026-09-09 13:22:30 -0600

    Update inputs, nixpkgs -> channels.nixos.org

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "(Lib)Canon dev environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
 
     perSystem = { pkgs, self', ... }: {
       packages.default = pkgs.rustPlatform.buildRustPackage {
